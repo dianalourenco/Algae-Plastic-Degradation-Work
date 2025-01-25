c999c742d6d11d73fd95f82bd46962b19af9e585
Diana Lourenço <dianalourenco14@gmail.com>
2025-01-25 17:51:57 +0000

    Add date and only add new sequences

--- NCBI_seq_retrieval.py
+++ NCBI_seq_retrieval.py
@@ -37,23 +37,51 @@ def save_seq_to_file(sequence_record, enzyme_name, species_name):
     '''
     Save sequence to a FASTA file
     '''
-    #Makes directory
-    os.makedirs('sequences', exist_ok=True)
+    #Make 'sequences' directory
+    base_dir = 'sequences'
+    os.makedirs(base_dir, exist_ok=True)
 
+    # Create subdirectory inside 'sequences' with date
+    date_dir = datetime.datetime.now().strftime('%G%m%d')
+    date_path = os.path.join(base_dir, date_dir)
+    os.makedirs(date_path, exist_ok = True)
+
+    # Create filepath for FASTA files
     filename = f'{enzyme_name}_{species_name}'.replace(' ', '')
-    filepath = os.path.join('sequences', f'{filename}.fasta')
+    filepath = os.path.join(date_path, f'{filename}.fasta')
 
     #Save sequence in FASTA format
     with open(filepath, 'w') as f:
         SeqIO.write(sequence_record,f,'fasta')
     return filepath
 
+def sequence_exists(enzyme, species):
+    '''
+    Check if sequence already exists in the directory
+    '''
+    base_dir = 'sequences'
+
+    for date_dir in os.listdir(base_dir): #iterates over subdirectories
+        date_path = os.path.join(base_dir, date_dir) #path: sequences/date
+        if os.path.isdir(date_path):
+            filename = f'{enzyme}_{species}'.replace(' ', '') + '.fasta'
+            filepath = os.path.join(date_path, filename)
+            if os.path.exists(filepath):
+                print('Sequence for the {enzyme} of {species} already exists.')
+                return True
+    return False
+
+
+
 def main(file_path):    
+    os.makedirs('sequences', exist_ok=True)
+
     with open(file_path,'r') as f:
         lines = [line.strip() for line in f if line.strip()]
 
     total_entries = len(lines[1:])
     processed = 0
+    new_seq = 0
 
     for line in lines[1:]:
         try:
@@ -61,15 +89,20 @@ def main(file_path):
             processed += 1
             print(f'\nProcessing ({processed}/{total_entries}): {enzyme_name} from {species_name}')
                 
+            # check if sequence exists
+            if sequence_exists(enzyme_name, species_name):
+                continue
+
             sequence_record = fetch_protein_sequence(enzyme_name, species_name)
 
             if sequence_record:
                 filepath = save_seq_to_file(sequence_record, enzyme_name,species_name)
                 print(f'Sequence saved to: {filepath}')
-            time.sleep(1)
+                new_seq += 1
         except Exception as e:
             print(f'Error in input text file: {e}')
-
+    
+    print(f'{new_seq} new sequences were found')
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
